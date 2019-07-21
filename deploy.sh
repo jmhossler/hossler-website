8f9b33e9841005fcaeda312e516fc7a217cf00d9
John Hossler <john.m.hossler@gmail.com>
2019-07-21 12:30:38 -0500

    chore: add some output to see if app.yaml is getting generated incorrectly

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-curl -sL https://github.com/digitalocean/doctl/releases/download/v1.22.0/doctl-1.22.0-linux-amd64.tar.gz | tar -xzv
-chmod +x ./doctl
-./doctl auth init
-./doctl kubernetes cluster kubeconfig save test-website-cluster
+# Publish docker stuff
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push jmhossler/hossler-website
 
+# Download kubectl and doctl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.14.4/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push jmhossler/hossler-website
+curl -sL https://github.com/digitalocean/doctl/releases/download/v1.22.0/doctl-1.22.0-linux-amd64.tar.gz | tar -xzv
+chmod +x ./doctl
+
+# Grab kubeconfig
+./doctl auth init
+./doctl kubernetes cluster kubeconfig save test-website-cluster
+
+# Generate kubernetes app.yaml file
 poetry install && poetry run generate_kubeconfig > app.yaml
+cat app.yaml
+
+# Deploy application
 ./kubectl apply -f app.yaml
