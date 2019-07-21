7f0f977b87ceeee468319612cde0bc5ebc1cbf35
John Hossler <john.m.hossler@gmail.com>
2019-07-21 12:23:04 -0500

    fix: install poetry application before generating config

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ chmod +x ./kubectl
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push jmhossler/hossler-website
-poetry run generate_kubeconfig > app.yaml
+poetry install && poetry run generate_kubeconfig > app.yaml
 ./kubectl apply -f app.yaml
