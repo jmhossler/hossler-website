1218251d3da569d193c7d5d1462635ece7cd3637
John Hossler <john.m.hossler@gmail.com>
2019-07-21 11:56:46 -0500

    fix: install kubectl and remove dumb test

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push jmhossler/hossler-website
 poetry run generate_kubeconfig > app.yaml
-kubectl apply -f app.yaml
+./kubectl apply -f app.yaml
--- get_kube_config.sh
+++ get_kube_config.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
 curl -sL https://github.com/digitalocean/doctl/releases/download/v1.22.0/doctl-1.22.0-linux-amd64.tar.gz | tar -xzv
+chmod +x ./doctl
 ./doctl auth init
 ./doctl kubernetes cluster kubeconfig save test-website-cluster
