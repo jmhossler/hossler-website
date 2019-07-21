fa16049d5ebf8ea6ff4030ffe9baad6fd5a86dc0
John Hossler <john.m.hossler@gmail.com>
2019-07-21 01:24:21 -0500

    fix: remove tagging the version

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push jmhossler/hossler-website:$(poetry run version | xargs)
+docker push jmhossler/hossler-website
