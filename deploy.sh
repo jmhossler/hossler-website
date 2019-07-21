71af39e21383535ddcc21cd0d6df49263d610a6c
John Hossler <john.m.hossler@gmail.com>
2019-07-21 12:18:01 -0500

    fix: generate kubeconfig in same script it is used and update website behavior

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+curl -sL https://github.com/digitalocean/doctl/releases/download/v1.22.0/doctl-1.22.0-linux-amd64.tar.gz | tar -xzv
+chmod +x ./doctl
+./doctl auth init
+./doctl kubernetes cluster kubeconfig save test-website-cluster
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.14.4/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push jmhossler/hossler-website
 poetry run generate_kubeconfig > app.yaml
